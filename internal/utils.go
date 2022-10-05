96cc75d5838eafeef096d7a5529f27edd37100e1
noot <elizabethjbinks@gmail.com>
2022-10-05 16:44:23 -0400

    cleanup

--- internal/utils.go
+++ internal/utils.go
@@ -6,9 +6,9 @@ import (
 
 func Sha256Multihash(mh multihash.Multihash) multihash.Multihash {
 	prefix := []byte("CR_DOUBLEHASH")
-	//dh := sha256.Sum256(append(prefix, mh...))
 	mh, err := multihash.Sum(append(prefix, mh...), multihash.DBL_SHA2_256, 30)
 	if err != nil {
+		// this shouldn't ever happen
 		panic(err)
 	}
 	return mh
