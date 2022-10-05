c0be529882aab394d0739f994777c138af2fb356
noot <elizabethjbinks@gmail.com>
2022-10-05 15:59:32 -0400

    add domain prefix to derived hash

--- internal/utils.go
+++ internal/utils.go
@@ -9,5 +9,6 @@ import (
 type Hash [32]byte
 
 func Sha256Multihash(mh multihash.Multihash) Hash {
-	return sha256.Sum256(mh)
+	prefix := []byte("CR_DOUBLEHASH")
+	return sha256.Sum256(append(prefix, mh...))
 }
