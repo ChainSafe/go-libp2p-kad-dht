4313318c3e62e6fb3a98030d76e10b30fdc260a6
noot <elizabethjbinks@gmail.com>
2022-10-19 12:50:45 -0400

    update Sha256Multihash to return last 32 bytes of multihash

--- internal/utils.go
+++ internal/utils.go
@@ -4,12 +4,14 @@ import (
 	"github.com/multiformats/go-multihash"
 )
 
+const keysize = 32
+
 func Sha256Multihash(mh multihash.Multihash) multihash.Multihash {
 	prefix := []byte("CR_DOUBLEHASH")
-	mh, err := multihash.Sum(append(prefix, mh...), multihash.DBL_SHA2_256, 30)
+	mh, err := multihash.Sum(append(prefix, mh...), multihash.DBL_SHA2_256, keysize)
 	if err != nil {
 		// this shouldn't ever happen
 		panic(err)
 	}
-	return mh
+	return mh[len(mh)-keysize:]
 }
